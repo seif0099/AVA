6d8787cd8dd6a1f7fe3cd0662a987b7f89e30b5e
khemiri22 <khe.amine22@gmail.com>
2023-12-02 22:17:35 +0100

    LoginAdmin

--- src/main/java/com/example/demo3/MainApp.java
+++ src/main/java/com/example/demo3/MainApp.java
@@ -11,8 +11,8 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("AVA");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,5 +8,11 @@ module com.example.demo3 {
     requires java.sql;
 
     opens com.example.demo3 to javafx.fxml;
+    opens com.example.demo3.Controllers to javafx.fxml;
     exports com.example.demo3;
+    exports com.example.demo3.Entities;
+    exports com.example.demo3.DAOs;
+    exports com.example.demo3.Controllers;
+    opens com.example.demo3.Entities to javafx.fxml;
+
 }
